fd6db59c885d273ffcdebcc15499027934e136ed
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-05-13 11:58:04 +0200

    LevelManager
    missing start scene, level manager

---
+++ Cars/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float carSpeed;
+	public float maxPos = 2.3f;
+	Vector3 position;
+
+	// Use this for initialization
+	void Start () 
+	{
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+		position.x = Mathf.Clamp(position.x, -2.4f, 2.4f);
+		transform.position = position;
+
+		position.y += Input.GetAxis("Vertical") * carSpeed * Time.deltaTime;
+		position.y = Mathf.Clamp(position.x, -4f, 3.2f);
+		transform.position = position;
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "EnemyCar")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
